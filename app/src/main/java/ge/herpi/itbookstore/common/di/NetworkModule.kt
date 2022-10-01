effbef244adcd74bdd49f6432a2047fa6ad34a86
george-gigauri <giorgi.gigauri.1@btu.edu.ge>
2022-10-01 17:51:04 +0400

    .

--- app/src/main/java/ge/herpi/itbookstore/common/di/NetworkModule.kt
+++ app/src/main/java/ge/herpi/itbookstore/common/di/NetworkModule.kt
@@ -1,15 +1,12 @@
 package ge.herpi.itbookstore.common.di
 
 import android.content.Context
-import androidx.room.Room
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import ge.herpi.itbookstore.common.db.AppDatabase
-import ge.herpi.itbookstore.details.data.local.dao.BookDetailsDao
 import ge.herpi.itbookstore.details.data.remote.api.BookDetailsApi
 import ge.herpi.itbookstore.details.data.repository.BookDetailsRepositoryImpl
 import ge.herpi.itbookstore.details.domain.repository.BookDetailsRepository
@@ -28,23 +25,48 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
+object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideAppContext(@ApplicationContext context: Context): Context = context
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(ChuckerInterceptor.Builder(context).build())
+            .build()
+    }
 
     @Provides
     @Singleton
-    fun provideDatabase(context: Context): AppDatabase =
-        Room.databaseBuilder(context, AppDatabase::class.java, "itbookstore")
-            .fallbackToDestructiveMigration().build()
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder().baseUrl("https://api.itbook.store/").client(client)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
 
     @Provides
     @Singleton
-    fun provideBookDetailsDao(db: AppDatabase): BookDetailsDao = db.bookDetailsDao()
+    fun provideBookService(retrofit: Retrofit): BookService {
+        return retrofit.create(BookService::class.java)
+    }
 
     @Provides
     @Singleton
-    fun provideSavedBooksDao(db: AppDatabase): SavedBooksDao = db.savedBooksDao()
+    fun provideBookRepository(api: BookService): BookRepository = BookRepositoryImpl(api)
+
+    @Provides
+    @Singleton
+    fun provideBookDetailsApi(retrofit: Retrofit): BookDetailsApi =
+        retrofit.create(BookDetailsApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideSearchApi(retrofit: Retrofit): SearchApi = retrofit.create()
+
+    @Provides
+    @Singleton
+    fun provideBookDetailsRepository(api: BookDetailsApi): BookDetailsRepository =
+        BookDetailsRepositoryImpl(api)
+
+    @Provides
+    @Singleton
+    fun provideSavedBooksRepository(db: SavedBooksDao): SavedBooksRepository =
+        SavedBooksRepositoryImpl(db)
 }
